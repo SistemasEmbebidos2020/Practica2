9d9d3c0102b28d28d49f29abe966e156dbcb932b
SistemasEmbebidos <78398897+SistemasEmbebidos2020@users.noreply.github.com>
2021-05-02 18:56:31 -0500

    Update led1.py

--- led1.py
+++ led1.py
@@ -1,12 +1,24 @@
+#!/usr/bin/env python3
+
 import RPi.GPIO as GPIO
-import time
+from time import sleep
 
 GPIO.setmode(GPIO.BCM)
+
 led1 = 6
+Bt1 = 5
+GPIO.setup(Bt1, GPIO.IN, GPIO.PUD_DOWN)
 GPIO.setup(led1, GPIO.OUT)
 
-for count in range(10):
+ 
+# Main function
+def main () :
+ for count in range(10):
   GPIO.output(led1,True)
-  time.sleep(1)
+  sleep(1)
   GPIO.output(led1,False)
-  time.sleep(1)
+  sleep(1)
+  
+# Command line execution
+if __name__ == '__main__' :
+   main()
