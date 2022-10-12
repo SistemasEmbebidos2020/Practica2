566cf89d7cf8b0ac590f26a96f0298e021934dfc
SistemasEmbebidos <78398897+SistemasEmbebidos2020@users.noreply.github.com>
2022-10-12 11:49:45 -0500

    Update led1.ino

--- led1.ino
+++ led1.ino
@@ -2,22 +2,26 @@
 void setup() {
 
 
-pinMode(12, OUTPUT); // pin 3 como salida 
-pinMode(13, OUTPUT); // pin 3 como salida 
+pinMode(12, OUTPUT); // pin 12 como salida 
+pinMode(13, OUTPUT); // pin 13 como salida también se puede usar LED_BUILDIN
 
 } 
 
 void loop() {
 
-
-  digitalWrite(12,HIGH); // pulsador presionado, encender LED
+//parapedea led pin 12
+  digitalWrite(12,HIGH); 
   delay(1000);
-  digitalWrite(12,LOW);  // pulsador no presionado, apagar LED
-  delay(1000);
-
-  digitalWrite(13,HIGH); // pulsador presionado, encender LED
-  delay(1000);
-  digitalWrite(13,LOW);  // pulsador no presionado, apagar LED
+  digitalWrite(12,LOW);  
   delay(1000);
 
+//parapedea led pin 12
+  
+//parapedea led pin 13  
+  digitalWrite(13,HIGH); 
+  delay(1000);
+  digitalWrite(13,LOW);  
+  delay(1000);
+
+//parapedea led pin 13
 }
