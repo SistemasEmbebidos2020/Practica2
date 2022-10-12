506f1e2edb894bd9c9c715c8c5d27ecca6b5fb43
SistemasEmbebidos <78398897+SistemasEmbebidos2020@users.noreply.github.com>
2022-10-12 11:16:38 -0500

    Update led1.ino

--- led1.ino
+++ led1.ino
@@ -1,20 +1,23 @@
 
 void setup() {
 
-pinMode(2, INPUT);  // pin 2 como entrada 
-pinMode(9, OUTPUT); // pin 3 como salida 
+
+pinMode(12, OUTPUT); // pin 3 como salida 
+pinMode(13, OUTPUT); // pin 3 como salida 
 
 } 
 
 void loop() {
 
-if (digitalRead(2) == HIGH) // evaluo si la entrada esta en nivel alto
-{ 
-  digitalWrite(9,HIGH); // pulsador presionado, encender LED
-} 
-else 
-{ 
-  digitalWrite(9,LOW);  // pulsador no presionado, apagar LED
-}
- 
+
+  digitalWrite(12,HIGH); // pulsador presionado, encender LED
+  delay(1000);
+  digitalWrite(12,LOW);  // pulsador no presionado, apagar LED
+  delay(1000);
+
+  digitalWrite(13,HIGH); // pulsador presionado, encender LED
+  delay(1000);
+  digitalWrite(13,LOW);  // pulsador no presionado, apagar LED
+  delay(1000);
+
 }
